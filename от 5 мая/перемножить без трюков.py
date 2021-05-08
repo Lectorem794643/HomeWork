6560e49b20879f99a44b594f911c96ce048064f4
Lectorem <79637710149@mail.ru>
2021-05-09 00:08:41 +0300

    .

---
+++ от 5 мая/перемножить без трюков.py
@@ -0,0 +1,8 @@
+# к сожалению он не дает называть переменные "soum" и \
+# "numeral" тк служебные команды
+
+suma = 1
+for _ in range(6):
+    nam = int(input('введите число'))
+    suma *= nam
+print(suma)
