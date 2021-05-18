cf7237ffec9002ae7b2158b09a9de64b9beb80f8
Lectorem <79637710149@mail.ru>
2021-05-18 21:47:59 +0300

    _

---
+++ от 12 мая/Школа танцев.py
@@ -0,0 +1,22 @@
+repeat = int(input())
+for i in range(repeat):
+    answer = 0
+    error = True
+    while error:
+        one = input()
+        if one == 'раз':
+            answer += 1
+            two = input()
+            if two == 'два':
+                answer += 1
+                three = input()
+                if three == 'три':
+                    answer += 1
+                    four = input()
+                    if four == 'четыре':
+                        answer += 1
+        else:
+            error = False
+    ans = str(answer)
+    print('Правильных отсчётов было', answer, ', но теперь вы ошиблись.')
+print('На сегодня хватит.')
