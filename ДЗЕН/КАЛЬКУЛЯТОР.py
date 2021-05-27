4acfb3edc774d3223a9b96579e910996ca4e9093
Lectorem <79637710149@mail.ru>
2021-05-27 09:48:39 +0300

    крутой калькулятор, но не оптимизированный

--- ДЗЕН/КАЛЬКУЛЯТОР.py
+++ ДЗЕН/КАЛЬКУЛЯТОР.py
@@ -1,45 +1,41 @@
-print('Инструкция - калькулятор запущен, для остановки введите "STOP"')
+print('Инструкция: калькулятор - запущен, для остановки введите "STOP"')
 STOP = True
 while STOP:
+    error_flag = True
     sign = input("Какую операцию необходимо провести (+,-,/,*, Factorial, %) :")
-    if sign != 'Factorial':
-        a = float(input('Введите первое число :'))
-        b = float(input("Введите второе число :"))
-        if sign == '+':
-            c = a + b
-            print(c)
-        elif sign == '-':
-            c = a - b
-            print(c)
-        elif sign == '/':
-            if b != 0:
-                c = a / b
-                print(c)
-            else:
-                print('На 0 делить нальзя!')
-        elif sign == '*':
-            c = a * b
-            print(c)
-        elif sign == 'Factorial':
-            factorial = 1
-            for i in range(1, a + 1):
-                factorial *= i
-                print(factorial)
-        elif sign == '%':
-            print(a % b)
-        elif sign == 'STOP':
-            STOP = False
-        else:
-            print('ERROR')
-            STOP = False
-        print('Операция завершена!')
-    else:
+    if sign != '+' and sign != '-' and sign != '/' and sign != '*' and sign != 'Factorial' and sign != '%':
+        print('НЕДОПУСТИМАЯ ОПЕРАЦИЯ')
+        print('ERROR')
+        error_flag = False
+    if error_flag:
         a = int(input('Введите первое число :'))
         if sign == 'Factorial':
             factorial = 1
             for i in range(1, a + 1):
                 factorial *= i
-            print(factorial)
-
-    print('Операция завершена!')
+            print('Факториал =', factorial)
+        else:
+            b = int(input("Введите второе число :"))
+            if sign == '+':
+                c = a + b
+                print('ответ =', c)
+            elif sign == '-':
+                c = a - b
+                print(c)
+            elif sign == '/':
+                if b != 0:
+                    c = a // b
+                    print('ответ =', c)
+                else:
+                    print('На 0 делить нальзя!')
+            elif sign == '*':
+                c = a * b
+                print('ответ =', c)
+            elif sign == '%':
+                print('ответ =', a % b)
+            elif sign == 'STOP':
+                STOP = False
+            print('Операция завершена!')
+    else:
+        print('Операция превана, RESTART')
 print('Работа окончена, спасибо!')
