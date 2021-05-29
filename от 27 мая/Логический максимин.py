4eb8e72f43b736be1bd01bf3707e3e7b1c7fcdac
Lectorem <79637710149@mail.ru>
2021-05-29 19:23:59 +0300

    правильнй (вроде) калькулятор, но не оптимизированный

---
+++ от 27 мая/Логический максимин.py
@@ -0,0 +1,15 @@
+number_of_roads = int(input('введите количество маршрутов : '))
+roads_X = 0
+height_MAX = 0
+for _ in range(number_of_roads):
+    subway = int(input('введите количество тоннелей на маршруте : '))
+    height_MIN = 1000000000
+# не думаю, что существуют более высокие тоннели
+    for __ in range(subway):
+        height = int(input('введите высоту тоннеля : '))
+        if height_MIN > height:
+            height_MIN = height
+    if height_MAX < height_MIN:
+        height_MAX = height_MIN
+        roads_X += 1
+print(roads_X, height_MAX)
