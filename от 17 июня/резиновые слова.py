480747418fdf34d6fb68c443f23577d4a2bc74e8
Lectorem <79637710149@mail.ru>
2021-06-22 21:57:40 +0300

    _-_-_

--- от 17 июня/резиновые слова.py
+++ от 17 июня/резиновые слова.py
@@ -1,14 +1,14 @@
 word = str(input())
 if len(word) % 2 == 0:
-    indent = int((len(word) - 2) / 2)
-    indent2 = -1
-    print(' ' * (indent + 1), word[int((len(word) - 2) / 2):int((len(word) - 2) / 2 + 2)])
-    q = (indent2 + 1)
-    for i in range(int((len(word) - 2) / 2)):
-        q -= 1
-        indent -= 2
-        indent2 -= 1
-        print(' ' * indent2, word[(indent2 + 1) - q], ' ' * indent, word[-(q - 1)])
+    print(' ' * int((len(word)) / 2 - 1), word[int(len(word) / 2) - 1:int(len(word) / 2) + 1])
+    counter = distance = int((len(word)) / 2 - 2)
+    counter_R = counter + 3
+    while counter >= 0:
+        print(' ' * (counter), word[counter], ((distance - counter) * 2) * ' ', word[counter_R])
+        counter -= 1
+        counter_R += 1
+if len(word) % 2 != 0:
+
 
 
 #  синхрофазатрон
