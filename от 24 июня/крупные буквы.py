2acbdf9c1e52fe741424bf37016540ecfa3764e5
Lectorem <79637710149@mail.ru>
2021-06-28 09:21:51 +0300

    _-_-_

---
+++ от 24 июня/крупные буквы.py
@@ -0,0 +1,20 @@
+n = input()
+for i in range(len(n)):
+    if n[i] == 'A':
+        print('    *     ')
+        print('   * *    ')
+        print('  *****   ')
+        print(' *      * ')
+        print('*        *')
+    if n[i] == 'B':
+        print('********  ')
+        print('*       * ')
+        print('********  ')
+        print('*       * ')
+        print('********  ')
+    if n[i] == 'C':
+        print(' ******** ')
+        print('*        *')
+        print('*         ')
+        print('*        *')
+        print(' ******** ')
