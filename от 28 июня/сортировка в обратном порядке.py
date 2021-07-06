231d292fe2cc2770ee4d7fdfdaeb382ecd00a862
Lectorem <79637710149@mail.ru>
2021-07-06 14:01:58 +0300

    почиинил ъ

--- от 28 июня/сортировка в обратном порядке.py
+++ от 28 июня/сортировка в обратном порядке.py
@@ -1,12 +1,11 @@
 list_of_numbers = []
 for _ in range(int(input())):
     list_of_numbers.append(int(input()))
-counter = 1
-while counter < len(list_of_numbers):
-    for i in range(len(list_of_numbers) - counter):
-        if list_of_numbers[i] < list_of_numbers[i-1]:
-            list_of_numbers[i], list_of_numbers[i-1] = list_of_numbers[i-1], list_of_numbers[i]
-    counter += 1
+for A in range(len(list_of_numbers)):
+    for B in range(A + 1, len(list_of_numbers)):
+        if list_of_numbers[A] < list_of_numbers[B]:
+            list_of_numbers[A], list_of_numbers[B] = list_of_numbers[B], list_of_numbers[A]
 for i in range(len(list_of_numbers)):
     print(list_of_numbers[i])
 
+# ПОЧИНИЛ
