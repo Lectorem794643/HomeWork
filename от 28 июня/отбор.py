55034f03fb5b53fe08489ec61147bf3ff24acd4c
Lectorem <79637710149@mail.ru>
2021-07-06 14:27:35 +0300

    почиинил ъ

--- от 28 июня/отбор.py
+++ от 28 июня/отбор.py
@@ -1,10 +1,20 @@
-cool_magazine = []
-good_students = []
-for i in range(int(input())):
-    new_students = [str(input()), int(input())]
+cool_magazine, good_students, good_good_students, new_students = list(), list(), list(), list()
+for i in range(int(input('введите количество учеников'))):
+    new_students = (str(input('введите фамилию и оценку ученика - ')))
     cool_magazine.append(new_students)
-    if new_students in (4, 5):
+    if '4' in new_students:
         good_students.append(new_students)
-print(cool_magazine)
+    if '5' in new_students:
+        good_good_students.append(new_students)
+print('классный журкал:')
+for i in range(len(cool_magazine)):
+    print(cool_magazine[i])
 print()
-print(good_students)
+print('отличники:')
+for i in range(len(good_good_students)):
+    print(good_good_students[i])
+print()
+print('хорошисты:')
+for i in range(len(good_students)):
+    print(good_students[i])
+
