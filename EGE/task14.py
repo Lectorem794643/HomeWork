10fc927d57eb9ddcea04e3197419029712eb20a7
Lectorem <79637710149@mail.ru>
2021-07-25 23:09:39 +0300

    выполнил дз на завтра

--- EGE/task14.py
+++ EGE/task14.py
@@ -1,4 +1,5 @@
 myExpression = 4 * 625 ** 9 - 25 ** 15 + 2 * 5 ** 11 - 7
+print(myExpression)
 countOfFours = 0
 while myExpression > 0:
     countOfFours += (myExpression % 5 == 4)  # объясни, что здесь происходит
